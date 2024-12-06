60271817350a69a903d652f009fb874fe2d99782
Nabin Thapa <151662784+Nabeen-Thapa@users.noreply.github.com>
2024-12-06 11:29:28 +0545

    linq basic

---
+++ linq_eg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_pratices
+{
+    internal class linq_eg
+    {
+        public static void Main()
+        {
+            int[] num = { 2, 5, 7, 8, 9, 13, 17, 1, 59 };  
+            IEnumerable<int> result = from n in num where n > 10 select n;
+            Console.WriteLine("result :");
+            foreach (int n in result) Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
